d55cab4be701382c43fc5dcdd2ead942c25b5525
spugia-ship-it <spugia@egrps.org>
2026-02-04 07:18:25 -0500

    Update fetch response handling in script.js
    Change response handling to display text in the document body.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRgyleyFfUTptH0_GBnXBm82dtIG3ovj2F6MY_uO-dqwf_hls-lOXbVZSo-bZgEJhQ_rlGgJYj5KBjT/pub?gid=0&single=true&output=csv")
-  .then(response => response.text())
+  .then(r => r.text())
   .then(text => {
-    console.log(text);
+    document.body.innerText = text;
   });
