c7fb6ff42c43fc3693bce87c3359c1b717b1bdb6
spugia-ship-it <spugia@egrps.org>
2026-02-06 11:39:59 -0500

    Fetch data from CSV and update bar heights

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const testHeights = [40, 80, 120, 160, 200, 100];
+const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRgyleyFfUTptH0_GBnXBm82dtIG3ovj2F6MY_uO-dqwf_hls-lOXbVZSo-bZgEJhQ_rlGgJYj5KBjT/pub?gid=0&single=true&output=csv";
 
-testHeights.forEach((value, i) => {
-  const bar = document.getElementById(`bar${i + 1}`);
-  bar.style.height = value + "px";
-});
+fetch(CSV_URL)
+  .then(res => res.text())
+  .then(text => {
+    const rows = text.trim().split("\n").map(r => r.split(","));
+
+    const totals = rows[0].slice(0, 6).map(Number);
+
+    totals.forEach((value, i) => {
+      const bar = document.getElementById(`bar${i + 1}`);
+      bar.style.height = value / 100000 + "px";
+    });
+  });
