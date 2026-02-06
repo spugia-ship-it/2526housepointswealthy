804a1e4a2dfee3cc45563937e7a79b5f9570acfe
spugia-ship-it <spugia@egrps.org>
2026-02-06 11:44:31 -0500

    Add console log to indicate script execution

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("script is running");
 const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRgyleyFfUTptH0_GBnXBm82dtIG3ovj2F6MY_uO-dqwf_hls-lOXbVZSo-bZgEJhQ_rlGgJYj5KBjT/pub?gid=0&single=true&output=csv";
 
 fetch(CSV_URL)
